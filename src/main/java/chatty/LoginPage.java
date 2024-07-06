45885d0d3d187a15ad92d690da70c104626d012f
sulida <sulida2011@gmail.com>
2024-07-06 17:23:54 +0300

    Add ignore

--- src/main/java/chatty/LoginPage.java
+++ src/main/java/chatty/LoginPage.java
@@ -1,8 +1,13 @@
 package chatty;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage{
+    @FindBy(id = "email")
+    private WebElement emailEditBox;
+
 
     public LoginPage(WebDriver driver) {
         super(driver);
